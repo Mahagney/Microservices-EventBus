a4ca9bf232e7de6deb9c3228a1837048c0bb0ec5
Saleh Mahagney <saleh.mahagney@ibm.com>
2021-04-04 13:07:48 +0300

    change name

--- event-bus/index.js
+++ event-bus/index.js
@@ -18,7 +18,7 @@ app.post("/events", (req, res) => {
     axios.post("http://localhost:4002/events", event);
     axios.post("http://localhost:4003/events", event);
   } catch (error) {
-    console.log(error);
+    console.log(error + " ");
   }
 
   res.send({ status: "ok" });
